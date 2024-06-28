1557ee1b1ff5cdb4efd5b7ed653c3df4863e5170
eric.roys@gmail.com <eric.roys@gmail.com>
2024-06-28 14:31:14 -0500

    make server work with routes and import

---
+++ server.js
@@ -0,0 +1,26 @@
+
+import express from 'express';
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { router } from './routes/index.js';
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// const api = require("./routes/index.js");
+const dir = path.dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use("/api", router);
+
+//serve the start page
+app.get("/", (req, res) =>
+  res.sendFile(path.join(dir, "/public/index.html"))
+);
+
+//serve the notes page
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(dir, "/public/notes.html"))
+);
+
+app.listen(PORT, () => console.info(`Running on port ${PORT}`));
